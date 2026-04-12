752a912067f95644ba27b351687e50563dbef09e
ALPHA09999 <bakourabood7@gmail.com>
2026-04-12 12:40:57 +0300

    feat: implement location store and endpoints for managing location data

--- src/components/common/LocationComponent.vue
+++ src/components/common/LocationComponent.vue
@@ -15,15 +15,19 @@
       <CustomQSelect
         :lst-data="locationStore.lstDistrict"
         label="المنطقة"
-        :get-data="() => locationStore.getDistrict(locationStore.lstGovernorate.selected?.id as number)"
+        :get-data="
+          () => locationStore.getDistrict(locationStore.lstGovernorate.selected?.id as number)
+        "
         :dependent-lists="[locationStore.lstSubdistrict]"
         :watch-value="locationStore.lstGovernorate.selected"
         :dependency-required="true"
         :reset-self-on-dependency-change="true"
         :clear-data-on-dependency-change="true"
-        @update:model-value="(val) => {
-          placeCode = locationStore.lstDistrict.selected?.code || '';
-        }"
+        @update:model-value="
+          (val) => {
+            placeCode = locationStore.lstDistrict.selected?.code || '';
+          }
+        "
         :rules="[(val) => !!val || 'يرجى اختيار المنطقة']"
         dense
         outlined
@@ -33,15 +37,19 @@
       <CustomQSelect
         :lst-data="locationStore.lstSubdistrict"
         label="الناحية"
-        :get-data="() => locationStore.getSubdistrict(locationStore.lstDistrict.selected?.id as number)"
+        :get-data="
+          () => locationStore.getSubdistrict(locationStore.lstDistrict.selected?.id as number)
+        "
         :dependent-lists="[locationStore.lstCommunity]"
         :watch-value="locationStore.lstDistrict.selected"
         :dependency-required="true"
         :reset-self-on-dependency-change="true"
         :clear-data-on-dependency-change="true"
-        @update:model-value="(val) => {
-          placeCode = locationStore.lstSubdistrict.selected?.code || '';
-        }"
+        @update:model-value="
+          (val) => {
+            placeCode = locationStore.lstSubdistrict.selected?.code || '';
+          }
+        "
         :rules="[(val) => !!val || 'يرجى اختيار الناحية']"
         dense
         outlined
@@ -51,28 +59,26 @@
       <CustomQSelect
         :lst-data="locationStore.lstCommunity"
         label="البلدة"
-        :get-data="() => locationStore.getCommunity(locationStore.lstSubdistrict.selected?.id as number)"
+        :get-data="
+          () => locationStore.getCommunity(locationStore.lstSubdistrict.selected?.id as number)
+        "
         :dependent-lists="[locationStore.lstSection]"
         :watch-value="locationStore.lstSubdistrict.selected"
         :dependency-required="true"
         :reset-self-on-dependency-change="true"
         :clear-data-on-dependency-change="true"
-        @update:model-value="(val) => {
-          placeCode = locationStore.lstCommunity.selected?.code || '';
-        }"
+        @update:model-value="
+          (val) => {
+            placeCode = locationStore.lstCommunity.selected?.code || '';
+          }
+        "
         :rules="[(val) => !!val || 'يرجى اختيار البلدة']"
         dense
         outlined
       />
     </div>
     <div class="col-12 col-md-3 hidden">
-      <q-input
-        v-model="placeCode"
-        label="رمز الموقع"
-        :readonly="true"
-        dense
-        outlined
-      />
+      <q-input v-model="placeCode" label="رمز الموقع" :readonly="true" dense outlined />
     </div>
   </div>
 </template>
---
+++ src/end-point/location-end-point.ts
@@ -0,0 +1,37 @@
+export const LOCATION_ENDPOINT = {
+  getGovernorate: {
+    route: 'getGovernorate',
+  },
+  getDistrict: {
+    route: 'getDistrict',
+    params: { id: Number },
+  },
+  getSubdistrict: {
+    route: 'getSubdistrict',
+    params: { id: Number },
+  },
+  getCommunity: {
+    route: 'getCommunity',
+    params: { id: Number },
+  },
+  getSection: {
+    route: 'getSection',
+    params: { id: Number },
+  },
+  getPart: {
+    route: 'getPart',
+    params: { id: Number },
+  },
+  getBuild: {
+    route: 'getBuild',
+    params: { id: Number },
+  },
+  getFloor: {
+    route: 'getFloor',
+    params: { id: Number },
+  },
+  getFlat: {
+    route: 'getFlat',
+    params: { id: Number },
+  },
+};
